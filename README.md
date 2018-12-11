fcee867d21a5a770f12669b5d3345ef0fa975129
speiris <shehanpeiris@gmail.com>
2018-12-11 16:35:58 -0600

    Final product, game complete

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # psychic_game
 Hwk 2 (easier assignment): Psychic game
+
+Welcome to Shehan's Psychic Game!
+When you load the page, the computer has chosen a letter. Can you guess the letter of which the computer is thinking?
+Type a letter and see if you're right...but beware! You only have 9 guesses. If you don't get it right in 9 guesses, you lose!
+
+Enjoy!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,50 @@
 // Create an array containing all the possible letters from which computer can choose its guess.
 var possibleLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-// Variable created to randomnly assign Computer's "choice"
+// Created a function to randomnly assign Computer's "choice".
 var cpuChoice = function() {
-    possibleLetters[Math.floor((Math.random())*26)];
+    return(possibleLetters[Math.floor((Math.random())*26)]);
     };
+
+// Run the cpuChoice function to have a computer guess for when user loads page. Console.log it as a test to make sure it's working properly.
+var cpuGuess = cpuChoice();
+console.log("Computer's guess is: " + cpuGuess);
 
 // Create base variables for the game.
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 
-// Creating a function that will run after user loses, increases loss count, resets guesses, alerts user to their loss, and updates loss text.
+// Creating a function that will run after user loses, increases loss count, resets guesses, alerts user to their loss, updates loss text and re-runs the cpuChoice function.
 var lossFunction = function() {
     losses++;
     alert("You lost!");
     $("#loss-text").text("Losses: " + losses);
     guesses = 9;
+    $("#guesses-left-text").text("Guesses left: " + guesses);
+    $("#guesses-text").text("Your guesses so far: ");
     cpuChoice();
+    cpuGuess = cpuChoice();
+    console.log("Computer's guess is: " + cpuGuess);
 }
 
-// Creating a function that will run after user wins, increases win count, resets guesses, alerts user to their victory, and updates win text.
+// Creating a function that will run after user wins, increases win count, resets guesses, alerts user to their victory, updates win text and re-runs cpuChoice function.
 var winFunction = function() {
     wins++;
     alert("Congrats! You won!");
     $("#win-text").text("Wins: " + wins);
     guesses = 9;
+    $("#guesses-left-text").text("Guesses left: " + guesses);
+    $("#guesses-text").text("Your guesses so far: ");
     cpuChoice();
+    cpuGuess = cpuChoice();
+    console.log("Computer's guess is: " + cpuGuess);
 }
 
-document.onkeyup = function(event) {
-
-    cpuChoice();
-    console.log("Computer's guess is: " + cpuChoice());
+document.onkeyup = function(event) {    
     var userGuess = event.key.toLowerCase();
     console.log("User's guess is: " + userGuess);
-    if (userGuess === cpuChoice) {
+    if (userGuess === cpuGuess) {
         winFunction();
     } else {
         guesses--;
